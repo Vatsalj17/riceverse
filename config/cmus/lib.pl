bec6cc90c4f7ad90dbaef7ef5056b83a297b6ee6
Vatsal Jaiswal <vatsaljaiswal17@gmail.com>
2025-03-23 12:35:56 +0530

    cmus config

---
+++ config/cmus/lib.pl
@@ -0,0 +1,62 @@
+/home/Vatsal/Music/Playlist/Aankhon Se Batana Dikshant Viral Song 2022 Official Video.mp3
+/home/Vatsal/Music/Playlist/Abhi Mujh Mein Kahin  Agneepath  Priyanka Chopra, Hrithik Roshan  Sonu Nigam  Ajay-Atul  4K.mp3
+/home/Vatsal/Music/Playlist/Agar Tum Saath Ho FULL AUDIO Song _ Tamasha _ Ranbir Kapoor, Deepika Padukone _ T-Series.mp3
+/home/Vatsal/Music/Playlist/Ajnabee - Bhuvan Bam _ Official Music Video _.mp3
+/home/Vatsal/Music/Playlist/Ali Zafar _ Jhoom (R&B mix) _ Lyrical Video (320K).mp3
+/home/Vatsal/Music/Playlist/Anuv Jain - BAARISHEIN (ACOUSTIC).mp3
+/home/Vatsal/Music/Playlist/Arcade x Mann Mera (Mashup) Full Version  Gravero.mp3
+/home/Vatsal/Music/Playlist/Arijit Singh_ Meet Full Video Song  Simran  Kangana Ranaut  Sachin-Jigar.mp3
+/home/Vatsal/Music/Playlist/A.R. Rahman, Lata Mangeshkar - Luka Chuppi (Lyrics) Rang De Basanti.mp3
+/home/Vatsal/Music/Playlist/Bann Gayi Zindagi.mp3
+/home/Vatsal/Music/Playlist/Barsaat Ki Dhun (320K).mp3
+/home/Vatsal/Music/Playlist/Bayaan, Hasan Raheem, Rovalio - Maand (Lyrics).mp3
+/home/Vatsal/Music/Playlist/BB Ki Vines- Bhuvan Bam- Bas Mein _ Official Music Video _.mp3
+/home/Vatsal/Music/Playlist/Behti Hawa Sa Tha Woh – 3 Idiots _ Aamir Khan, Madhavan, Sharman J _ Shaan & Shantanu M _  (320K).mp3
+/home/Vatsal/Music/Playlist/Bekhayali Full Song  Kabir Singh  Shahid K,Kiara ASandeep Reddy Vanga  Sachet-Parampara  Irshad.mp3
+/home/Vatsal/Music/Playlist/Bhuvan Bam - Safar (Lyrics).mp3
+/home/Vatsal/Music/Playlist/Bhuvan Bam- Sang Hoon Tere _ Official Music Video _.mp3
+/home/Vatsal/Music/Playlist/Bhuvan Bam- Teri Meri Kahaani _ Official Music Video _.mp3
+/home/Vatsal/Music/Playlist/Breathless  Shankar Mahadevan  Javed Akhtar  Official Music Video.mp3
+/home/Vatsal/Music/Playlist/Chaand Baaliyan (320K).mp3
+/home/Vatsal/Music/Playlist/Dikshant - Aankhon Se Batana (Official Audio) (320K).mp3
+/home/Vatsal/Music/Playlist/Dil Ko Karaar Aaya - Sidharth Shukla & Neha Sharma _ Neha Kakkar & YasserDesai _ Rajat Nagpal _ Rana.mp3
+/home/Vatsal/Music/Playlist/Gori Teri Aankhen  Lucky Ali  Unacademy Unwind With MTV.mp3
+/home/Vatsal/Music/Playlist/Heeriye X Chaleya X Tu Aake Dekhle Insta Mashup  revibe  Viral Insta Reels, TikTok Remix.mp3
+/home/Vatsal/Music/Playlist/Heer Ranjha - Bhuvan Bam _ Official Music Video _.mp3
+/home/Vatsal/Music/Playlist/Jaadugari Music Video _ Jitendra Kumar, Arushi Sharma _ Nilotpal Bora _ Jaadugar _ Netflix (320K).mp3
+/home/Vatsal/Music/Playlist/Jab Koi Baat  500 Miles   (Mashup  Cover)  Rishikesh Kamerkar.mp3
+/home/Vatsal/Music/Playlist/Justh - Chor (Official Music Video) (320K).mp3
+/home/Vatsal/Music/Playlist/Kaun Hain Voh (320K).mp3
+/home/Vatsal/Music/Playlist/KAUN TUJHE (320K).mp3
+/home/Vatsal/Music/Playlist/Kesariya - Brahmāstra _ Ranbir Kapoor _ Alia Bhatt _ Pritam _ Arijit Singh _ Amitabh Bhatt (320K).mp3
+/home/Vatsal/Music/Playlist/King - Maan Meri Jaan  (Lyrics).mp3
+/home/Vatsal/Music/Playlist/Kon Disa Mein - Full Song   Ravindra Jain  Varsha Singh Dhanoa  Maddy Sharma  Guru Dhanoa.mp3
+/home/Vatsal/Music/Playlist/laree Choote lyrics Original (320K).mp3
+/home/Vatsal/Music/Playlist/Let Her Go x Husn (Gravero Mashup)  Anuv Jain.mp3
+/home/Vatsal/Music/Playlist/Let Me Down Slowly x Main Dhoondne Ko Zamaane Mein (Gravero Mashup)  Full Version.mp3
+/home/Vatsal/Music/Playlist/Maine Royaan _ Lofi_Remix_ Tanveer Evan..mp3
+/home/Vatsal/Music/Playlist/Main Rang Sharbaton Ka [Slowed+Reverb] - Arijit Singh _ Music lovers _ Textaudio.mp3
+/home/Vatsal/Music/Playlist/Mann Mera (Lyrics HD) - Table No. 21 ft. Gajendra Verma _ FULL Song (320K).mp3
+/home/Vatsal/Music/Playlist/Mat kar maya ko Ahankar - (full song)  Scam 1992-The harshad mehta story end emotional scene song.mp3
+/home/Vatsal/Music/Playlist/Mere Nishaan Full Song  Oh My God   Akshay Kumar, Paresh Rawal  Kailash Kher, Meet Bros Anjjan.mp3
+/home/Vatsal/Music/Playlist/Mere Samne Wali Khidki Mein  Ashish Patil  Padosan  Kishore Kumar  Cover  2018 HD.mp3
+/home/Vatsal/Music/Playlist/Naino ki ghaat le ja naino ki naiya (Tum Tak) (slowed _ reverb) _ But it's a 10 minute loo (320K)-[AudioTrimmer.com].mp3
+/home/Vatsal/Music/Playlist/O SANAM - SUNOH   live UNDP Paytm insider.-[AudioTrimmer.com].mp3
+/home/Vatsal/Music/Playlist/Pal Lyric Video - Jalebi_Arijit Singh_Shreya Ghoshal_Rhea & Varun_Javed - Mohsin (320K).mp3
+/home/Vatsal/Music/Playlist/Piya Ghar Aavenge in HD - Kailash on Coke Studio @ MTV S01.mp3
+/home/Vatsal/Music/Playlist/Ranjha (320K).mp3
+/home/Vatsal/Music/Playlist/Ritviz - JEET [Official Audio] (320K).mp3
+/home/Vatsal/Music/Playlist/Ritviz - Udd Gaye [Official Audio] (320K).mp3
+/home/Vatsal/Music/Playlist/Saazish.mp3
+/home/Vatsal/Music/Playlist/Sapna Jahan - Lyric Video  Brothers  Akshay Kumar  Jacqueline Fernandez.mp3
+/home/Vatsal/Music/Playlist/Tera Ban Jaunga (320K).mp3
+/home/Vatsal/Music/Playlist/Tera Chehra_Jaan Meri_Ep- 1_Tulsi_Jubin_T-Series MixtapeRewindSeason 3_Abhijit V lAhmed K  (320K).mp3
+/home/Vatsal/Music/Playlist/Tera Yaar Hoon Main (320K).mp3
+/home/Vatsal/Music/Playlist/Teri Yaadon Mein (Full Song)  The Killer.mp3
+/home/Vatsal/Music/Playlist/Tu Hain Toh - Lyric VideoMr. & Mrs. Mahi Rajkummar, Janhvi  Hunny, Bunny, Sagar.mp3
+/home/Vatsal/Music/Playlist/Tujhe Kitna Chahne Lage (From _Kabir Singh_) (320K).mp3
+/home/Vatsal/Music/Playlist/Tum Hi Aana (From _Marjaavaan_) (320K).mp3
+/home/Vatsal/Music/Playlist/Tu Mile Dil Khile (320K).mp3
+/home/Vatsal/Music/Playlist/TUM MERE - FUKRA INSAAN Ft.Crazydeep (OFFICIAL MUSIC VIDEO ) !! My FIRST LOVE SONG.mp3
+/home/Vatsal/Music/Playlist/YUBA DSA - YEDA YUNG  INSTRUMENTAL  Prod. by YD.mp3
+/home/Vatsal/Music/Playlist/Zara Zara (320K).mp3
